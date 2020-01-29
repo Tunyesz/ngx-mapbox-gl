fac1c119aacbdea71c1ea7ce6c5dd74157d1c5b0
Peter Varga <tunyesz@gmail.com>
2020-01-29 09:18:41 +0100

    Build init

--- projects/showcase/src/app/doc/doc.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  templateUrl: './doc.component.html',
-  styleUrls: ['./doc.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class DocComponent { }
